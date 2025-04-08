7432df276dd9e7df9fb595a84946f49b7c483cce
puboy7 <110157302+puboy7@users.noreply.github.com>
2025-04-08 02:42:28 +0000

    Deploying to gh-pages from @ puboy7/LILYGO.github.io@b0541c4c3f667be6744c5956132d31d0de86696d 🚀

--- static/js/plugin_blog/main.js
+++ static/js/plugin_blog/main.js
@@ -88,7 +88,7 @@ $(document).ready(function(){
         });
     }
     if(isBlog){
-        downloadJson("/static/blog_index/index.json", onDownloadOk);
+        downloadJson("/LILYGO.github.io/static/blog_index/index.json", onDownloadOk);
     }
 });
 
